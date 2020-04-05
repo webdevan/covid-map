542e4e35eb807c5e2e2dd8abfde82d55a339cc83
webdevan <gitlab@webdevan.co.za>
2020-04-05 11:33:41 +0000

    improve styling/color algorithms

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 main();
 
+// const mapType = 'none';
 // const mapType = 'count';
 // const mapType = 'change';
 const mapType = 'percentageChange';
@@ -75,7 +76,7 @@ async function main () {
   const lastDate = columns.pop();
   const dateString = new Intl.DateTimeFormat('en-ZA', { month: 'long', day: 'numeric', year: 'numeric' }).format(new Date(date));
   if (mapType === 'count') {
-    document.querySelector('.map-title').innerHTML = `All cases in South Africa (${dateString})`;
+    document.querySelector('.map-title').innerHTML = `Confirmed cases in South Africa (${dateString})`;
   } else if (mapType === 'change') {
     document.querySelector('.map-title').innerHTML = `Increase in cases in South Africa (${dateString})`;
   } else if (mapType === 'percentageChange') {
@@ -100,11 +101,9 @@ async function main () {
     if (!region.map) return;
 
     // draw map polygon
-    // const weightedCount = region.count;
-    const weightedCount = region.count / region.population * 1000000;
-    // const weightedCount = region.count / region.area * 1000;
-    // const weightedCount = Math.pow(region.count / region.area, 0.75) * 1000;
-    const opacity = 0.1 * Math.sqrt(weightedCount) / 2;
+    // const weightedValue = 255 - Math.min(255, region.count);
+    const weightedValue = 255 - Math.min(255, Math.pow(region.count / region.population * 10000000, 0.8));
+    // const weightedValue = 255 - Math.min(255, region.count / region.area * 1000);
     let points = [];
     if (region.map.type === 'Polygon') {
       points = region.map.coordinates[0].map(point => [point[1], point[0]]);
@@ -112,32 +111,32 @@ async function main () {
       points = region.map.coordinates.map(coordinates => coordinates[0].map(point => [point[1], point[0]]));
     }
     const poly = L.polygon(points, {
-      color: '#cccccc44',
-      fillColor: '#000000ff',
-      fillOpacity: opacity,
+      color: `rgba(150, 150, 150, 0.1)`,
+      fillColor: `rgba(${weightedValue}, ${weightedValue}, ${weightedValue}, 1)`,
+      fillOpacity: 0.5,
     });
     // poly.bindTooltip(`${region.count} +${region.change}`, {permanent: true, direction:"center"}).openTooltip();
     poly.addTo(map);
 
     // draw map marker
     let size = 22;
-    let color = '#ff000066';
+    let color = '#dd000066';
     let label = region.count;
     if (mapType === 'count') {
       if (region.count === 0) return;
-      size = 30 + region.count / 10;
+      size = 22 + region.count / 10;
       label = region.count;
     } else if (mapType === 'change') {
       if (region.change === 0) return;
       size = 30 + Math.pow(Math.abs(region.change), 1.5) / 3;
-      if (region.change < 0) color = '#00ff0066';
+      if (region.change < 0) color = '#00dd0077';
       label = `${region.change > 0 ? '+' : ''}${region.change}`;
     } else if (mapType === 'percentageChange') {
       if (region.change === 0) return;
-      size = 50 + Math.pow(Math.abs(region.change), 1.5) / 3;
-      if (region.change < 0) color = '#00ff0066';
+      size = 48 + Math.pow(Math.abs(region.change), 1.5) / 3;
+      if (region.change < 0) color = '#00dd0077';
       const percent = region.change / (region.count - region.change) * 100;
-      label = `${region.change > 0 ? '+' : ''}${Math.round(percent)}%`;
+      label = `${percent > 0 ? '+' : ''}${Math.round(percent)}%`;
     } else return;
     const icon = L.divIcon({
       className: 'custom-div-icon',
