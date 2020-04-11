42ccc38da5fccec200fb7f7b8c33da9716df902b
webdevan <gitlab@webdevan.co.za>
2020-04-11 15:23:58 +0000

    small refactor and add more map types/display options

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 main();
 
-// const mapType = 'none';
-const mapType = 'count';
-// const mapType = 'change'; // broken
-// const mapType = 'percentageChange'; // broken
+var
+  map,
+  mapMarkers,
+  mapPolygons,
+  mapType,
+  regions,
+  provincesZa,
+  subdistrictsZa,
+  subdistrictsCpt,
+  provincialInfections,
+  westernCapeInfections,
+  provincialData,
+  westernCapeData,
+  dataDateText;
 
 function fetchCsv(url) {
   return fetch(url)
@@ -36,6 +46,16 @@ async function fetchData() {
     // western capes data
     fetchCsv('https://raw.githubusercontent.com/dsfsi/covid19za/master/data/district_data/provincial_wc_cumulative.csv'),
   ])
+  .then(res => {
+    return [
+      regions,
+      provincesZa,
+      subdistrictsZa,
+      subdistrictsCpt,
+      provincialInfections,
+      westernCapeInfections,
+    ] = res;
+  })
   .catch(err => {
     alert('Oops! Something is wrong.');
     console.error(err);
@@ -43,15 +63,14 @@ async function fetchData() {
 }
 
 function createMap() {
-  const map = L.map('map', {
+  map = L.map('map', {
     center: [-28.806460, 24.936116],
     zoom: 6,
     minZoom: 5,
     maxZoom: 11,
     attributionControl: false,
   });
-  const tiles = new L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(map);
-  return map;
+  new L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_all/{z}/{x}/{y}.png').addTo(map);
 }
 
 function toRagColor(value) {
@@ -62,53 +81,39 @@ function toRagColor(value) {
   return `rgb(${red}, ${green}, ${blue})`;
 }
 
-async function main () {
-  const map = createMap();
+function bindMapControls() {
+  const mapTypeSelect = document.querySelector('.map-controls select');
+  mapType = mapTypeSelect.value;
+  mapTypeSelect.addEventListener('input', event => {
+   mapType = mapTypeSelect.value;
+   changeMapType();
+  }, false);
+}
 
-  const data = await fetchData();
-
-  // processData
-  const [
-    regions,
-    provincesZa,
-    subdistrictsZa,
-    subdistrictsCpt,
-    provincialInfections,
-    westernCapeInfections,
-  ] = data;
-
-  // determine latest data dates
-  let date, provincialData, westernCapeData;
-  for (let i = provincialInfections.length; i > 0; i--) {
-    provincialData = provincialInfections[i - 1];
-    if (Object.values(provincialData).some(item => item === null)) continue;
-    date = provincialData.date;
-    westernCapeData = westernCapeInfections.find(item => item.date === date);
-    if (!westernCapeData) continue;
-    if (Object.values(westernCapeData).some(item => item === null)) continue;
-    break;
-  };
-  
-  if (!date) return alert('Oops. Invalid data set!');
-  dateArray = date.split('-');
-  const dateString = new Intl.DateTimeFormat('en-ZA', { month: 'long', day: 'numeric', year: 'numeric' }).format(new Date(`${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`));
+function changeMapType() {
+  // remove old map
+  if (mapMarkers) mapMarkers.forEach(marker => map.removeLayer(marker));
+  mapMarkers = [];
+  if (mapPolygons) mapPolygons.forEach(polygon => map.removeLayer(polygon));
+  mapPolygons = [];
 
   // set map title
   if (mapType === 'count') {
-    document.querySelector('.map-title').innerHTML = `Confirmed cases in South Africa (${dateString}) | Source: <a href="https://github.com/dsfsi/covid19za" target="_blank">https://github.com/dsfsi/covid19za</a>`;
+    document.querySelector('.map-title').innerHTML = `Confirmed cases, ${dataDateText} | Source: <a href="https://github.com/dsfsi/covid19za" target="_blank">https://github.com/dsfsi/covid19za</a>`;
   } else if (mapType === 'change') {
-    return alert('BROKEN');
-    document.querySelector('.map-title').innerHTML = `Increase in cases in South Africa (${dateString}) | Source: <a href="https://github.com/dsfsi/covid19za" target="_blank">https://github.com/dsfsi/covid19za</a>`;
-  } else if (mapType === 'percentageChange') {
-    return alert('BROKEN');
-    document.querySelector('.map-title').innerHTML = `Increase in cases in South Africa (${dateString}) | Source: <a href="https://github.com/dsfsi/covid19za" target="_blank">https://github.com/dsfsi/covid19za</a>`;
+    document.querySelector('.map-title').innerHTML = `New cases, ${dataDateText} | Source: <a href="https://github.com/dsfsi/covid19za" target="_blank">https://github.com/dsfsi/covid19za</a>`;
+  } else if (mapType === 'changePercent') {
+    document.querySelector('.map-title').innerHTML = `New cases (%), ${dataDateText} | Source: <a href="https://github.com/dsfsi/covid19za" target="_blank">https://github.com/dsfsi/covid19za</a>`;
   }
 
+  const provincialDataYesterday = provincialInfections[provincialInfections.indexOf(provincialData) - 1];
+  const westernCapeDataYesterday = westernCapeInfections[westernCapeInfections.indexOf(westernCapeData) - 1];
   regions.forEach(region => {
 
     // assign infections stats
     region.count = provincialData[region.region_id] || westernCapeData[region.region_id];
-    // region.change = regionData[date] - regionData[lastDate]; // TODO
+    region.countYesterday = provincialDataYesterday[region.region_id] || westernCapeDataYesterday[region.region_id];
+    region.change = region.count - region.countYesterday;
     
     // assign map polygon
     const maps = {
@@ -136,6 +141,7 @@ async function main () {
     });
     // poly.bindTooltip(`${region.count} +${region.change}`, {permanent: true, direction:"center"}).openTooltip();
     poly.addTo(map);
+    mapPolygons.push(poly);
 
     // draw map marker
     let size = 22;
@@ -151,11 +157,11 @@ async function main () {
       size = 30 + Math.pow(Math.abs(region.change), 1.5) / 3;
       if (region.change < 0) color = '#00dd0077';
       label = `${region.change > 0 ? '+' : ''}${region.change}`;
-    } else if (mapType === 'percentageChange') {
+    } else if (mapType === 'changePercent') {
       if (region.change === 0) return;
-      size = 48 + Math.pow(Math.abs(region.change), 1.5) / 3;
-      if (region.change < 0) color = '#00dd0077';
       const percent = region.change / (region.count - region.change) * 100;
+      size = 48 + Math.pow(Math.abs(percent), 1.5) / 3;
+      if (region.change < 0) color = '#00dd0077';
       label = `${percent > 0 ? '+' : ''}${Math.round(percent)}%`;
     } else return;
     const icon = L.divIcon({
@@ -178,5 +184,32 @@ async function main () {
     });
     const position = poly.getCenter();
     const marker = L.marker(position, { icon }).addTo(map);
-  });
+    mapMarkers.push(marker);
+  });  
+}
+
+function determineDataDates() {
+  // determine latest data dates
+  let date;
+  for (let i = provincialInfections.length; i > 0; i--) {
+    provincialData = provincialInfections[i - 1];
+    if (Object.values(provincialData).some(item => item === null)) continue;
+    date = provincialData.date;
+    westernCapeData = westernCapeInfections.find(item => item.date === date);
+    if (!westernCapeData) continue;
+    if (Object.values(westernCapeData).some(item => item === null)) continue;
+    break;
+  };
+  if (!date) return alert('Oops. Invalid data set!');
+  dateArray = date.split('-');
+  const dataDate = new Date(`${dateArray[2]}-${dateArray[1]}-${dateArray[0]}`);
+  dataDateText = new Intl.DateTimeFormat('en-ZA', { month: 'long', day: 'numeric', year: 'numeric' }).format(dataDate);
+}
+
+async function main () {
+  bindMapControls();
+  createMap();
+  await fetchData();
+  determineDataDates();
+  changeMapType();
 }
